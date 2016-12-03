0815ef3a6c69a34cc1d614612d987a20708536c4
Brian Faust <hello@brianfaust.de>
2016-12-03 19:31:19 +0200

    :hammer: Start making use of some PHP 7.1 goodies

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -9,11 +9,12 @@
  * file that was distributed with this source code.
  */
 
+declare(strict_types=1);
+
 namespace BrianFaust\Tests\Doorkeeper;
 
 use GrahamCampbell\TestBench\AbstractPackageTestCase;
 
 abstract class AbstractTestCase extends AbstractPackageTestCase
 {
-    //
 }
